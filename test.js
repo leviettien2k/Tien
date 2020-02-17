7ee9be4058154b988cdc446f76e9db32a85879b1
leviettien2k <59875837+leviettien2k@users.noreply.github.com>
2020-02-17 19:21:59 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-var danhsach =[{name:"Le Viet Tien",{mssv:51801029}]
+var danhsach =[{name:"Le Viet Tien",mssv:51801029},
+               {name:"Vo Phuc Khang",mssv:51800688},
+               {name:"Nguyen Minh Hung",mssv:51800893}]
